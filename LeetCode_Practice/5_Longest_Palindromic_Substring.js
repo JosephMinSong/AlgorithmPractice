9162ed05f183a508ff418a3951dd17c82e67978a
Joseph Song <song.josephmin@gmail.com>
2023-08-16 16:19:57 -0700

    algos

---
+++ LeetCode_Practice/5_Longest_Palindromic_Substring.js
@@ -0,0 +1,46 @@
+/**
+ * @param {string} s
+ * @return {string}
+ */
+var longestPalindrome = function (s) {
+    let longest = 0;
+    let l_i = 0;
+    let r_i = 0;
+
+    for (let i = 0; i < s.length; i++) {
+
+        // checking odd length palindromes
+        let l = i;
+        let r = i;
+
+        while (l >= 0 && r < s.length && s[l] == s[r]) {
+            const length = r - l + 1;
+
+            if (length > longest) {
+                l_i = l;
+                r_i = r;
+                longest = length
+            }
+            l--
+            r++
+        }
+
+        // checking even length palindromes
+        l = i
+        r = i + 1
+
+        while (l >= 0 && r < s.length && s[l] == s[r]) {
+            const length = r - l + 1;
+
+            if (length > longest) {
+                l_i = l;
+                r_i = r;
+                longest = length
+            }
+            l--
+            r++
+        }
+    }
+
+    return s.substring(l_i, r_i + 1)
+};
