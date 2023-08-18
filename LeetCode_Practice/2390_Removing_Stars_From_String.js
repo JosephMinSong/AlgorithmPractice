6134c79a3b73df3bb255c27bdbb8e7e1c4b6d0d9
Joseph Song <song.josephmin@gmail.com>
2023-08-17 23:09:58 -0700

    stack solution

--- LeetCode_Practice/2390_Removing_Stars_From_String.js
+++ LeetCode_Practice/2390_Removing_Stars_From_String.js
@@ -20,3 +20,21 @@ var removeStars = function (s) {
 
     return result
 };
+
+/**
+ * @param {string} s
+ * @return {string}
+ */
+var removeStars = function (s) {
+    let stack = [];
+
+    for (let i = 0; i < s.length; i++) {
+        if (s[i] == "*") {
+            stack.pop()
+        } else {
+            stack.push(s[i])
+        }
+    }
+
+    return stack.join("")
+};
