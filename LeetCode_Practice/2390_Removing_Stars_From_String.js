e160ca62ca4ba86dd515ce1154d4c643987b5fb4
Joseph Song <song.josephmin@gmail.com>
2023-08-17 23:06:43 -0700

    algos

---
+++ LeetCode_Practice/2390_Removing_Stars_From_String.js
@@ -0,0 +1,22 @@
+/**
+ * @param {string} s
+ * @return {string}
+ */
+var removeStars = function (s) {
+    let result = "";
+    let starCount = 0;
+
+    for (let i = s.length - 1; i >= 0; i--) {
+        if (s[i] == "*") {
+            starCount++
+        } else {
+            if (starCount > 0) {
+                starCount--
+            } else {
+                result = s[i] + result
+            }
+        }
+    }
+
+    return result
+};
