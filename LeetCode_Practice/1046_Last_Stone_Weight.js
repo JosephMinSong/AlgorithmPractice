332d897d1f385e44b370f8bc7b2f68e8920095cd
JosephMinSong <song.josephmin@gmail.com>
2023-08-25 13:24:43 -0700

    algos

--- LeetCode_Practice/1046_Last_Stone_Weight.js
+++ LeetCode_Practice/1046_Last_Stone_Weight.js
@@ -20,4 +20,32 @@ var lastStoneWeight = function(stones) {
     return stones[0]
 };
 
+/**
+ * @param {number[]} stones
+ * @return {number}
+ */
+var lastStoneWeight = function(stones) {
+    if(stones.length == 1) return stones[0]
+
+    while(stones.length > 1){
+        stones = stones.sort((a, b) => a - b);
+        
+        const length = stones.length;
+
+        const diff = stones[length - 1] - stones[length - 2];
+
+        if(diff){
+            stones[length - 2] = diff;
+        } else {
+            stones.pop()
+        }
+
+        stones.pop()
+    }
+
+    return stones.length > 0 ? stones[0] : 0
+};
+
+// [1, 1, 2, 4, 7, 8]
+
 // [1, 1, 2, 4, 7, 8]
