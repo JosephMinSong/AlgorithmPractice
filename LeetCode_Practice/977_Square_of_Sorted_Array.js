8a5528363d2787d4e301d02a580745db514902c0
JosephMinSong <song.josephmin@gmail.com>
2023-08-26 11:12:58 -0700

    algos

--- LeetCode_Practice/977_Square_of_Sorted_Array.js
+++ LeetCode_Practice/977_Square_of_Sorted_Array.js
@@ -5,3 +5,32 @@
 var sortedSquares = function(nums) {
     return nums.map(num => Math.pow(num, 2)).sort((a, b) => a-b)
 };
+
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+var sortedSquares = function(nums) {
+    let result = [];
+    let left = 0;
+    let right = nums.length - 1;
+
+    for(let i = nums.length - 1; i >= 0; i--){
+
+        const rightNum = nums[right];
+        const leftNum = nums[left];
+        let square;
+
+        if(Math.abs(rightNum >= Math.abs(leftNum))){
+            square = Math.pow(rightNum, 2);
+            right--
+        } else {
+            square = Math.pow(leftNum, 2);
+            left++
+        }
+
+        result[i] = square
+    }
+
+    return result
+};
