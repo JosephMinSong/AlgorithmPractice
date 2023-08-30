4dba11aa5916f1c4f765b0c963c4056725205cf2
Joseph Song <song.josephmin@gmail.com>
2023-08-29 17:55:01 -0700

    algos

--- LeetCode_Practice/506_Relative_Ranks.js
+++ LeetCode_Practice/506_Relative_Ranks.js
@@ -3,15 +3,14 @@
  * @return {string[]}
  */
 var findRelativeRanks = function (score) {
-    let original = [...score]
-    const sortedScores = score.sort((a, b) => b - a); // descending order of scores
+    const sortedScores = [...score].sort((a, b) => b - a); // descending order of scores
     let scoresHash = {};
     const ans = [];
     for (let i = 0; i < sortedScores.length; i++) {
         scoresHash[sortedScores[i]] = i + 1
     }
 
-    for (let num of original) {
+    for (let num of score) {
         if (scoresHash[num] == 1) ans.push("Gold Medal")
         else if (scoresHash[num] == 2) ans.push("Silver Medal")
         else if (scoresHash[num] == 3) ans.push("Bronze Medal")
