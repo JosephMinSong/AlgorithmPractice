4cebf0fd982b2a4c6799657120a3ef3a0a50dd26
Joseph Song <song.josephmin@gmail.com>
2023-08-30 18:49:55 -0700

    algos

--- LeetCode_Practice/232_Implement_Queue_using_Stacks.js
+++ LeetCode_Practice/232_Implement_Queue_using_Stacks.js
@@ -54,7 +54,7 @@ MyQueue.prototype.empty = function () {
 /** 
  * Your MyQueue object will be instantiated and called as such:
  * var obj = new MyQueue()
- * obj.push(x)
+ * obj.push(x)c
  * var param_2 = obj.pop()
  * var param_3 = obj.peek()
  * var param_4 = obj.empty()
