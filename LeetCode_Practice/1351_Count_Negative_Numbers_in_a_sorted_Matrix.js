458626dcfb61c8b13edd166b7115fdd3591b1c9b
Joseph Song <song.josephmin@gmail.com>
2023-08-31 11:45:54 -0700

    algos

---
+++ LeetCode_Practice/1351_Count_Negative_Numbers_in_a_sorted_Matrix.js
@@ -0,0 +1,19 @@
+/**
+ * @param {number[][]} grid
+ * @return {number}
+ */
+var countNegatives = function (grid) {
+    let ans = 0;
+
+    for (let i = 0; i < grid.length; i++) {
+        for (let j = grid[i].length - 1; j >= 0; j--) {
+            if (grid[i][j] < 0) {
+                ans++
+            } else {
+                break
+            }
+        }
+    }
+
+    return ans
+};
