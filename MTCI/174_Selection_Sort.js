4637879e02f22b19c2e58a446a09682f043abd73
Joseph Song <song.josephmin@gmail.com>
2023-09-06 18:24:12 -0700

    algos

---
+++ MTCI/174_Selection_Sort.js
@@ -0,0 +1,20 @@
+const numbers = [99, 44, 6, 2, 1, 5, 63, 87, 283, 4, 0];
+
+function selectionSort(array) {
+    for (let i = 0; i < array.length - 1; i++) {
+        let min = i;
+        for (let j = i + 1; j < array.length; j++) {
+            if (array[j] < array[min]) {
+                min = j
+            }
+        }
+        if (i !== min) {
+            const temp = array[min];
+            array[min] = array[i];
+            array[i] = temp
+        }
+    }
+    return array
+}
+
+console.log(selectionSort(numbers));
