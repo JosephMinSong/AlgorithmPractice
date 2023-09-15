839cd523cf6642e164b8dc883feb586dec6d55bb
Joseph Song <song.josephmin@gmail.com>
2023-09-15 00:13:45 -0700

    algos

--- LeetCode_Practice/146_LRU_Cache.js
+++ LeetCode_Practice/146_LRU_Cache.js
@@ -23,8 +23,7 @@ class LinkedList {
 
     // Node(-1, -1) <--> newNode <--> Node(-1, -1)
 
-    add(key, value) {
-        const newNode = new Node(key, value);
+    add(newNode) {
         const prevTail = this.tail.prev;
         prevTail.next = newNode; // the previous tails next is now the new node
         newNode.prev = prevTail; // the new nodes previous was the previous tail
@@ -35,7 +34,10 @@ class LinkedList {
     }
 
     remove(node) {
-
+        node.prev.next = node.next;
+        node.next.prev = node.prev;
+        this.length--
+        return node
     }
 }
 
@@ -50,7 +52,15 @@ var LRUCache = function (capacity) {
  * @return {number}
  */
 LRUCache.prototype.get = function (key) {
-
+    // check our map to see if we have the key
+    if (this.map[key]) {
+        const node = this.map[key]
+        this.queue.remove(node); // move it to the back since we used it
+        this.queue.add(node);
+        return node.value
+    } else {
+        return -1
+    }
 };
 
 /** 
@@ -61,6 +71,30 @@ LRUCache.prototype.get = function (key) {
 LRUCache.prototype.put = function (key, value) {
     // first check if we're at capacity, if we're not, simply add the key value pair into our map and add it to the back of the queue
     // if we're at capacity, then remove the first item from the queue, then remove that key from the map
+
+    // check to see if we already have this in our map
+    if (this.map[key]) {
+        // we can just remove it and put it in the back
+        this.queue.remove(this.map[key]) // remove the node from the queue
+        let newNode = new Node(key, value) // add the new node to the queue
+        this.queue.add(newNode);
+        this.map[key] = newNode; // update the map
+        return // break
+    }
+
+    // check to see if we are at capacity
+    // if we are, then we have to delete the first item in our queue
+    if (this.queue.length == this.capacity) {
+        let firstNode = this.queue.head.next;
+        this.queue.remove(firstNode)
+        delete this.map[firstNode.key]
+    }
+
+    // add the new node to the queue and map
+    let newNode = new Node(key, value);
+    this.queue.add(newNode)
+    this.map[key] = newNode;
+    return
 };
 
 /** 
