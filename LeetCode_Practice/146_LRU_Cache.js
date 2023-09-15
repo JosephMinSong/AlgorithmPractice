5a6bbf819b9398d11a4616aece25ed10b6e9a0ff
Joseph Song <song.josephmin@gmail.com>
2023-09-14 23:24:21 -0700

    algos

--- LeetCode_Practice/146_LRU_Cache.js
+++ LeetCode_Practice/146_LRU_Cache.js
@@ -1,12 +1,48 @@
 /**
  * @param {number} capacity
  */
-class LRUCache {
-    constructor(capacity) {
-        this.capacity = capacity;
-        this.map = {}
-        this.queue = new LinkedList()
+
+// Classes to use
+class Node {
+    constructor(key, value) {
+        this.key = key;
+        this.value = value;
+        this.next = null;
+        this.prev = null;
     }
+}
+
+class LinkedList {
+    constructor() {
+        this.head = new Node(-1, -1); // this way we can implement our remove function better
+        this.tail = new Node(-1, -1);
+        this.head.next = this.tail;
+        this.tail.prev = this.head;
+        this.length = 0;
+    }
+
+    // Node(-1, -1) <--> newNode <--> Node(-1, -1)
+
+    add(key, value) {
+        const newNode = new Node(key, value);
+        const prevTail = this.tail.prev;
+        prevTail.next = newNode; // the previous tails next is now the new node
+        newNode.prev = prevTail; // the new nodes previous was the previous tail
+        newNode.next = this.tail; // the new nodes next is the tail (-1, -1) node
+        this.tail.prev = newNode; // the tail node's previous is the new node
+        this.length++
+        return this
+    }
+
+    remove(node) {
+
+    }
+}
+
+var LRUCache = function (capacity) {
+    this.capacity = capacity;
+    this.map = {};
+    this.queue = new LinkedList()
 };
 
 /** 
@@ -14,18 +50,7 @@ class LRUCache {
  * @return {number}
  */
 LRUCache.prototype.get = function (key) {
-    // first in, first out methodology == queue
-    // but if we use it, then that gets put to the back
-    // if key exists, we store it as recently used, meaning we put it to the back
 
-    if (this.map[key]) {
-        const currentNode = this.map[key]
-        // move the node to the back
-        this.queue.moveToBack(currentNode);
-        return currentNode.value // returns the value
-    } else {
-        return -1
-    }
 };
 
 /** 
@@ -34,102 +59,9 @@ LRUCache.prototype.get = function (key) {
  * @return {void}
  */
 LRUCache.prototype.put = function (key, value) {
-    // if at capacity, get rid of front of queue and then add node to back of queue and to map
-    if (!this.map[key]) { // if it doesn't exist 
-        if (this.queue.length >= this.capacity) { // and we are at capacity - we must remove the LRU
-            let removedNode = this.queue.removeFront()
-            delete this.map[removedNode.key]
-        }
-        const newNode = new Node(key, value)
-        this.map[key] = newNode
-        this.queue.addToBack(newNode)
-        return
-    } else { // if we do have it in our map
-        let updatedNode = this.map[key]
-        updatedNode.value = value; // update the value in our map
-        this.queue.moveToBack(updatedNode)
-        return
-    }
+    // first check if we're at capacity, if we're not, simply add the key value pair into our map and add it to the back of the queue
+    // if we're at capacity, then remove the first item from the queue, then remove that key from the map
 };
-
-// doubly linked list
-class LinkedList {
-    constructor() {
-        this.head = null;
-        this.tail = null;
-        this.length = 0;
-    }
-
-    addToBack(node) {
-        if (this.length == 0) {
-            this.head = node;
-            this.tail = node;
-            this.length++
-            return this
-        } else {
-            this.tail.next = node; // set the tails' next to the new node
-            node.prev = this.tail; // set the new nodes prev to the tail (previously last one)
-            this.tail = node // set the new tail to the new node
-            this.length++
-            return this
-        }
-    }
-
-    moveToBack(node) {
-        if (this.length == 1 || this.length == 0 || this.tail == node) {
-            return
-        } else {
-            if (!node.prev) {
-                // meaning this is the head
-                this.head = node.next; // new head is the next one
-            } else {
-                // if we're somewhere in the middle
-                node.prev.next = node.next;
-                node.next.prev = node.prev;
-            }
-            this.tail.next = node; // put this at the back
-            node.prev = this.tail; // set the prev to the tail
-            node.next = null; // set the new next to null
-            this.tail = node; // set the new tail to the node
-            return
-        }
-    }
-
-    removeFront() {
-        if (this.length == 0) {
-            return
-        }
-        let removedNode = this.head; // store it temporarily to return it
-        this.head = removedNode.next; // move the head up one spot
-        removedNode.next = null; // remove the pointer from the previous head
-        if (this.head) {
-            this.head.prev = null; // set the new head's previous to null to get rid of the pointer
-        }
-        this.length--
-        return removedNode
-    }
-
-    printList() {
-        let runner = this.head;
-        let count = 1
-        console.log("Printing the new list!")
-        while (runner != null) {
-            console.log(count + " " + runner.key + " " + runner.value)
-            runner = runner.next
-        }
-        return
-    }
-}
-
-class Node {
-    constructor(key, value) {
-        this.key = key
-        this.value = value
-        this.next = null
-        this.prev = null
-    }
-}
-
 
 /** 
  * Your LRUCache object will be instantiated and called as such:
@@ -137,4 +69,3 @@ class Node {
  * var param_1 = obj.get(key)
  * obj.put(key,value)
  */
-
