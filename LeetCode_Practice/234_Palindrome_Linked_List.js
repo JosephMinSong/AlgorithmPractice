8671c751fc5dc17a61a7f6b2521a39ba1de07d86
Joseph Song <song.josephmin@gmail.com>
2023-09-16 15:07:25 -0700

    algos

--- LeetCode_Practice/234_Palindrome_Linked_List.js
+++ LeetCode_Practice/234_Palindrome_Linked_List.js
@@ -30,3 +30,58 @@ var isPalindrome = function (head) {
 
     return true
 }
+
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {boolean}
+ */
+
+//  1 --> 2 --> 3 --> 4 --> 5
+//              S                    F
+
+// 1 --> 2 <-- 3 <-- 4
+//       S           F
+var isPalindrome = function (head) {
+    // fast and slow runner method
+    // starting at the slow runner, we reverse the list in place, using the slow runner as the first "previous"
+
+    let slow = head;
+    let fast = head.next;
+    while (fast != null && fast.next != null) {
+        fast = fast.next.next;
+        slow = slow.next
+    }
+
+    // now reverse the linked list in place
+    let previous = null;
+    let start = slow.next;
+    while (start) {
+        const nextUp = start.next;
+        start.next = previous;
+        previous = start
+        start = nextUp
+    }
+
+
+    // start at both ends and see if they're matching
+    let runner1 = head;
+    let runner2 = previous;
+    while (runner1 && runner2) {
+        if (runner1.val != runner2.val) {
+            return false
+        } else {
+            runner1 = runner1.next;
+            runner2 = runner2.next;
+        }
+    }
+
+    return true
+
+}
